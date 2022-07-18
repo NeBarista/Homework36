f331d56dd44010e3486ec0aa8fa279127bab337c
NeBarista <tishkinsergey123@gmail.com>
2022-07-19 01:27:15 +0300

    My Homework

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        numberDevided(9);
+    }
+    public static void numberDevided(int max) {
+        for(int a = 0; a<max; a +=3) {
+            System.out.println(a);
+        }
+    }
+}
